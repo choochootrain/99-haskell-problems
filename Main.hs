9f5a7586767d7aab1299f4b69b2265f6925df982
Hurshal Patel <hpatel516@gmail.com>
2014-02-10 18:11:57 -0800

    add problem 2

--- Main.hs
+++ Main.hs
@@ -3,3 +3,10 @@ myLast :: [a] -> a
 myLast [] = error "list is empty"
 myLast (head : []) = head
 myLast (_ : tail) = myLast tail
+
+
+-- 2. Find the last but one element of a list.
+myButLast :: [a] -> a
+myButLast [] = error "list is too short"
+myButLast (x : []) = error "list is too short"
+myButLast x = myLast $ init x
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 ===================
 
 1. Find the last element of a list
+2. Find the last but one element of a list
