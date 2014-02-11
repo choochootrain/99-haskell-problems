f085a9a68b5b6d35c074183c73941f98f7089cd5
Hurshal Patel <hpatel516@gmail.com>
2014-02-10 18:24:04 -0800

    add problem 3

--- Main.hs
+++ Main.hs
@@ -10,3 +10,10 @@ myButLast :: [a] -> a
 myButLast [] = error "list is too short"
 myButLast (x : []) = error "list is too short"
 myButLast x = myLast $ init x
+
+
+-- 3. Find the K'th element of a list. The first element in the list is number 1.
+elementAt :: [a] -> Int -> a
+elementAt list x = if x <= length list && x > 0
+                      then list !! (x - 1)
+                      else error "index out of bounds"
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 1. Find the last element of a list
 2. Find the last but one element of a list
+3. Find the K'th element of a list. The first element in the list is number 1.
