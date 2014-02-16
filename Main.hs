8f0f3a93be972aa1c1d9892d09100aa2de3ca8db
Hurshal Patel <hpatel516@gmail.com>
2014-02-16 13:00:51 -0800

    add problem 10

--- Main.hs
+++ Main.hs
@@ -60,3 +60,11 @@ pack x = foldr (\x y -> if x /= head y !! 0
                           then [[x]] ++ y
                           else ([(head y ++ [x])] ++ tail y))
                [[last x]] $ init x
+
+
+-- 10. Run-length encoding of a list. Use the result of problem P09 to implement
+-- the so-called run-length encoding data compression method. Consecutive
+-- duplicates of elements are encoded as lists (N E) where N is the number of
+-- duplicates of the element E.
+encode :: (Eq a) => [a] -> [(Int, a)]
+encode = (map (\x -> (length x, head x))) . pack
--- README.md
+++ README.md
@@ -10,3 +10,4 @@
 7. Flatten a nested list structure.
 8. Eliminate consecutive duplicates of list elements.
 9. Pack consecutive duplicates of list elements into sublists. If a list contains repeated elements they should be placed in separate sublists.
+10. Run-length encoding of a list. Use the result of problem P09 to implement the so-called run-length encoding data compression method. Consecutive duplicates of elements are encoded as lists (N E) where N is the number of duplicates of the element E.
