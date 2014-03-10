c0755c9c132e835f7522e8ed78244e2d2fa4f153
Hurshal Patel <hpatel516@gmail.com>
2014-03-10 11:13:44 -0700

    add problem 40

--- Main.hs
+++ Main.hs
@@ -300,3 +300,19 @@ betterTotient n = foldr (*) 1 $ map product $ primeFactorsMult n
 -- upper limit, construct a list of all prime numbers in that range.
 primesR :: Int -> Int -> [Int]
 primesR n m = filter isPrime [n..m]
+
+
+-- 40. Goldbach's conjecture. Goldbach's conjecture says that every positive
+-- even number greater than 2 is the sum of two prime numbers.
+-- Example: 28 = 5 + 23. It is one of the most famous facts in number theory
+-- that has not been proved to be correct in the general case. It has been
+-- numerically confirmed up to very large numbers (much larger than we can go
+-- with our Prolog system). Write a predicate to find the two prime numbers
+-- that sum up to a given even integer.
+goldbach :: Int -> (Int, Int)
+goldbach n = (a, b)
+  where
+    pair = \x -> (isPrime x) && (isPrime $ n - x)
+    rems = filter pair [2..n-2]
+    a = head rems
+    b = n - a
--- README.md
+++ README.md
@@ -37,3 +37,4 @@
 36. Determine the prime factors of a given positive integer. Construct a list containing the prime factors and their multiplicity.
 37. Calculate Euler's totient function phi(m) (improved).
 39. A list of prime numbers. Given a range of integers by its lower and upper limit, construct a list of all prime numbers in that range.
+40. Goldbach's conjecture. Goldbach's conjecture says that every positive even number greater than 2 is the sum of two prime numbers. Example: 28 = 5 + 23. It is one of the most famous facts in number theory that has not been proved to be correct in the general case. It has been numerically confirmed up to very large numbers (much larger than we can go with our Prolog system). Write a predicate to find the two prime numbers that sum up to a given even integer.
