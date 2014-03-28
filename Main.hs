f5d1d030031a6c9e9ccb21ee7f7b85329b30fd48
Hurshal Patel <hpatel516@gmail.com>
2014-03-28 14:40:25 -0700

    add problem 71

--- Main.hs
+++ Main.hs
@@ -773,3 +773,13 @@ stringToMtree s = case parse parseMtree "" s of
       n <- many parseMtree
       char '^'
       return $ (Node x n)
+
+
+-- 71. Determine the internal path length of a tree. We define the internal
+-- path length of a multiway tree as the total sum of the path lengths from the
+-- root to all nodes of the tree. By this definition, tree5 has an internal
+-- path length of 9.
+ipl :: MTree a -> Int
+ipl (Node _ n)
+  | length n == 0 = 0
+  | otherwise     = sum $ map (\t -> ipl t + nnodes t) n
